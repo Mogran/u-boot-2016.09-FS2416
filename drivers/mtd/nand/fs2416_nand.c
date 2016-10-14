3fabc812ace8f2fd23a550e97664050822354e10
mogran <lumingliang.github@gmail.com>
2016-10-14 17:07:35 +0800

    initial commit

--- drivers/mtd/nand/fs2416_nand.c
+++ drivers/mtd/nand/fs2416_nand.c
@@ -11,41 +11,48 @@
 #include <asm/arch/s3c24x0_cpu.h>
 #include <asm/io.h>
 
-#define S3C2410_NFCONF_EN          (1<<15)
-#define S3C2410_NFCONF_512BYTE     (1<<14)
-#define S3C2410_NFCONF_4STEP       (1<<13)
-#define S3C2410_NFCONF_INITECC     (1<<5)
 #define S3C2410_NFCONF_nFCE        (1<<1)
-#define S3C2410_NFCONF_TACLS(x)    ((x)<<8)
-#define S3C2410_NFCONF_TWRPH0(x)   ((x)<<4)
-#define S3C2410_NFCONF_TWRPH1(x)   ((x)<<0)
 
-#define S3C2410_ADDR_NALE 4
-#define S3C2410_ADDR_NCLE 8
+
+static void s3c2416_clear_Rnb(void)
+{
+	struct s3c24x0_nand *nand = s3c24x0_get_base_nand();
+
+	writel(readl(&nand->nfstat) | (1 << 4), &nand->nfstat);
+}
 
 static void s3c24x0_hwcontrol(struct mtd_info *mtd, int cmd, unsigned int ctrl)
 {
 	struct nand_chip *chip = mtd_to_nand(mtd);
 	struct s3c24x0_nand *nand = s3c24x0_get_base_nand();
 
-	debug("hwcontrol(): 0x%02x 0x%02x\n", cmd, ctrl);
-
 	if (ctrl & NAND_CTRL_CHANGE) {
 
-		if (!(ctrl & NAND_CLE))
+		if ((ctrl & NAND_CLE)){
+			debug("hwcontrol(): write command : 0x%02x \n", cmd);
 			writeb(cmd, &nand->nfcmmd);
-		if (!(ctrl & NAND_ALE))
+			if(cmd == NAND_CMD_RESET || cmd == NAND_CMD_ERASE2 
+				|| cmd == NAND_CMD_READSTART || cmd == NAND_CMD_PAGEPROG)
+			{
+				s3c2416_clear_Rnb();
+			}
+		}
+		if ((ctrl & NAND_ALE)){
+			debug("hwcontrol(): write addr : 0x%02x \n", cmd);
 			writeb(cmd, &nand->nfaddr);
+		}
 
-		if (ctrl & NAND_NCE)
-			writel(readl(&nand->nfconf) & ~S3C2410_NFCONF_nFCE,
-			       &nand->nfconf);
-		else
-			writel(readl(&nand->nfconf) | S3C2410_NFCONF_nFCE,
-			       &nand->nfconf);
+		if (ctrl & NAND_NCE){
+			writel(readl(&nand->nfcont) & ~S3C2410_NFCONF_nFCE,
+			       &nand->nfcont);
+		}else{
+			debug("hwcontrol() disselect chip \n");
+			writel(readl(&nand->nfcont) | S3C2410_NFCONF_nFCE,
+			       &nand->nfcont);
+		}
 	}
 
-	if (cmd != NAND_CMD_NONE)
+	if (cmd != NAND_CMD_NONE && cmd != 0x03)
 		writeb(cmd, chip->IO_ADDR_W);
 }
 
@@ -53,8 +60,10 @@ static int s3c24x0_dev_ready(struct mtd_info *mtd)
 {
 	struct s3c24x0_nand *nand = s3c24x0_get_base_nand();
 
-	debug("dev_ready\n");
-	return readl(&nand->nfstat) & 0x01;
+	/*while(!(readl(&nand->nfstat)&(1 << 4)));*/
+
+	/* debug("dev_ready\n");*/
+	return (readl(&nand->nfstat)&(1 << 4));
 }
 
 int board_nand_init(struct nand_chip *nand)
@@ -64,12 +73,8 @@ int board_nand_init(struct nand_chip *nand)
 	debug("board_nand_init()\n");
 
 	/* initialize nand_chip data structure */
-#if 0	
 	writel((0<<12)|(2<<8)|(1<<4)|(1<<1), &nand_reg->nfconf);
-#else
-	writel((7<<12)|(7<<8)|(7<<4)|(1<<1), &nand_reg->nfconf);
-#endif
-	writel((2<<12)|(1<<1)|(1<<0), &nand_reg->nfcont);
+	writel((0<<8)|(1<<1)|(1<<0), &nand_reg->nfcont);
 
 	nand->IO_ADDR_R = (void *)&nand_reg->nfdata;
 	nand->IO_ADDR_W = (void *)&nand_reg->nfdata;
@@ -79,8 +84,8 @@ int board_nand_init(struct nand_chip *nand)
 	/* hwcontrol always must be implemented */
 	nand->cmd_ctrl = s3c24x0_hwcontrol;
 	nand->dev_ready = s3c24x0_dev_ready;
-	nand->ecc.mode = NAND_ECC_SOFT;
-
+	nand->ecc.mode = NAND_ECC_NONE;
+	nand->options = 0;
 
 	debug("end of nand_init\n");
 
